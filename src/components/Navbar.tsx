ba2dda74cb7738e7eb92d70c5ead7385945e9512
Sez-Lattana <lattana@hellosezame.com>
2023-06-29 18:42:06 +0200

    [ADD] padding bottom

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
 
   return (
-    <div className="sticky top-0 z-10">
+    <div className="sticky top-0 z-10 pb-12">
       <div className="mx-auto px-2 bg-primary sm:px-4 lg:px-8 shadow-lg py-2">
         <div className="relative flex items-center justify-between h-16">
           <div className="md:flex items-center lg:px-0 flex-1">
