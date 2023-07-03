ee42b9fbc03ae3f3e94c032fb78e9820e621e544
Sez-Lattana <lattana@hellosezame.com>
2023-07-03 11:45:43 +0200

    [ADD] zoospace link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 
-
 export default function Navbar() {
-
   return (
     <div className="sticky top-0 z-10 pb-12">
       <div className="mx-auto px-2 bg-primary sm:px-4 lg:px-8 shadow-lg py-2">
         <div className="relative flex items-center justify-between h-16">
           <div className="md:flex items-center lg:px-0 flex-1">
-            <button>
-              <a href="/">
-                <h1 className="text-gray-300">Home</h1>
-              </a>
-            </button>
+            <Link href="/">
+              <h1 className="text-gray-300">Home</h1>
+            </Link>
           </div>
           <div className="hidden md:flex items-center flex-1 justify-evenly">
             <Link href="/login">
@@ -21,6 +17,9 @@ export default function Navbar() {
 
             <Link href="/register">
               <h1 className="text-gray-300">Register</h1>
+            </Link>
+            <Link href="/spaces/spaces">
+              <h1 className="text-gray-300">Zoo&apos;s Spaces</h1>
             </Link>
           </div>
         </div>
